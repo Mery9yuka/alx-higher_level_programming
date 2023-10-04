5ff3cacbedbe24f57050e4aff8b466d4cd4729be
Mery9yuka <benhaida.maryem@gmail.com>
2023-10-04 23:04:14 +0100

    lots of loops

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for y in range(1, 101):
+        if y % 5 == 0 and y % 3 == 0:
+            print("FizzBuzz ", end='')
+        elif y % 5 == 0:
+            print("Buzz ", end='')
+        elif y % 3 == 0:
+            print("Fizz ", end='')
+        else:
+            print("{} ".format(y), end="")
