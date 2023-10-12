9a0d7e001156bfe388d93ab6d31bde74f1fb160b
Mery9yuka <benhaida.maryem@gmail.com>
2023-10-12 03:27:54 +0100

    matrix

--- 0x04-python-more_data_structure/0-square_matrix_simple.py
+++ 0x04-python-more_data_structure/0-square_matrix_simple.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-
-    for y in range(len(a_list)):
-        print("{:d}".format(a_list[y]))
+    return ([list(map(lambda x: x * x, y)) for y in matrix])
