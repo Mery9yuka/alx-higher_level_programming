9230201596ebd06009eddf8a9820dfd3f95156ef
Mery9yuka <benhaida.maryem.com>
2023-10-23 22:52:46 +0100

    divide 2 int

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+
+    try:
+        results = a / b
+        print("Inside result: {}".format(results))
+        return (results)
+
+    except ZeroDivisionError:
+        return (None)
