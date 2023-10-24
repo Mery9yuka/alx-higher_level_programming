05ee01502825abf3e3af88d850bdb029161b2f98
Mery9yuka <benhaida.maryem.com>
2023-10-25 00:35:52 +0100

    it will works

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,21 +3,18 @@
 
 
 class Square:
-    """Define instance of the Square class with given size"""
+    """Defining a  constructor size of a square"""
     def __init__(self, size=0):
-        """define a new square
+        """initialize a new square
 
-        size, is the size of the new square we defined
+        size is the size of new square we define
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-
         self.__size = size
 
-        def area(self):
-            """
-            Returns the area of the square.
-            """
-            return self.__size ** 2
+    def area(self):
+        """Returns the area of the square."""
+        return (self.__size ** 2)
