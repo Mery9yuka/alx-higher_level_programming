5b670760203f0bd59ce42827e1caef7cf7100ae5
Mery9yuka <benhaida.maryem.com>
2023-10-25 00:04:32 +0100

    square 0

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defining a class square"""
+
+
+class Square:
+    """Define instance of the Square class with given size"""
+    def __init__(self, size):
+        """inititialize the Variables"""
+        self.__size = size
+
