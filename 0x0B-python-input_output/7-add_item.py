e77e14fff3e51ed6b5b007be851a6a9402c4a7d5
Mery9yuka <benhaida.maryem.com>
2023-11-07 22:35:47 +0100

    json4

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""scripts adding all arguments to a Python list, and then saving them to a file"""
+
+import sys
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file  \
+        = __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        arguments = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        arguments = []
+    arguments.extend(sys.argv[1:])
+    save_to_json_file(arguments, "add_item.json")
