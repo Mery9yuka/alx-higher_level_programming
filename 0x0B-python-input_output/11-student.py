a0551f0d77a2d01396cc278c2532939ba0e96a7c
Mery9yuka <benhaida.maryem.com>
2023-11-07 23:07:07 +0100

    json11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """creates  a Student class """
 
+
 class Student:
     """ Retrieves a dictionary representation of student instance"""
     def __init__(self, first_name, last_name, age):
