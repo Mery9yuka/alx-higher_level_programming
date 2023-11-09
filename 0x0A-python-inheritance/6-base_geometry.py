8e00425a238d6b2c4862a2812a0e3c22805ad6ba
Mery9yuka <benhaida.maryem.com>
2023-11-09 01:12:12 +0100

    inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defining a new geometry class"""
+
+
+class BaseGeometry:
+    """ the class BaseGeometry """
+    def area(self):
+        """No implementation """
+        raise Exception("area() is not implemented")
