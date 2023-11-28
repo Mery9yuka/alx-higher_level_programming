450ccf58fcf05b16fa4c00b657f9fbff653c30e8
Mery9yuka <benhaida.maryem@gmail.com>
2023-11-28 01:04:50 +0100

    tasks up late

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -3,17 +3,17 @@
 
 
 def print_square(size):
-	"""Printing a square with the char #.
-		Raises:
-TypeError: If the size is not an int.
-	ValueError: If size is less than 0
-	 """
-	 if not isinstance(size, int):
-		 raise TypeError("size must be an integer")
-		 if size < 0:
-		 raise ValueError("size must be >= 0")
+    """Printing a square with the char #.
+    Raises:
+        TypeError: If the size is not an int.
+        ValueError: If size is less than 0
+    """
 
-		 for y in range(size):
-			 [print("#", end="") for u in range(size)]
-			  print("")
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
 
+    for y in range(size):
+        [print("#", end="") for u in range(size)]
+        print("")
