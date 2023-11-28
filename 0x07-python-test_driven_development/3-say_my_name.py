6365c9a7d95417a8f7391d45c9d4e74f0456a0cc
Mery9yuka <benhaida.maryem@gmail.com>
2023-11-28 01:15:51 +0100

    tasks up late

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,26 +1,14 @@
 #!/usr/bin/python3
-"""Defining a function that divide all elements in matrix."""
+"""Defineing a function that prints name."""
 
 
-def matrix_divided(matrix, div):
-    """Defining a function that divide all elements in matrix..
-    Returns:
-        the new matrix that represent the result of the divided elements
+def say_my_name(first_name, last_name=""):
+    """function that Prints a name.
+    Raises:
+        TypeError: If the first_name or last_name are not strings.
     """
-    if (not isinstance(matrix, list) or matrix == [] or
-            not all(isinstance(R, list) for R in matrix) or
-            not all((isinstance(ele, int) or isinstance(ele, float))
-                    for ele in [N for R in matrix for N in R])):
-        raise TypeError("matrix must be a matrix (list of lists) of "
-                        "integers/floats")
-
-    if not all(len(R) == len(matrix[0]) for R in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
-
-    if not isinstance(div, int) and not isinstance(div, float):
-        raise TypeError("div must be a number")
-
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-
-    return ([list(map(lambda x: round(x / div, 2), R)) for R in matrix])
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
