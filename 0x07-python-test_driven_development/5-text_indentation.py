36435e7cdbe7916db386462cc4e9fcf6c9516b24
Mery9yuka <benhaida.maryem@gmail.com>
2023-11-28 01:08:31 +0100

    tasks up late

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,25 +3,24 @@
 
 
 def text_indentation(text):
-	"""Print a text with 2 new lines after each of these char -> '.', '?', and ':'.
-		Raises:
-TypeError: If text isn't a string.
-	"""
-	 if not isinstance(text, str):
-		 raise TypeError("text must be a string")
+    """Print a text with 2 new lines after each of these char -> '.', '?', and ':'.
+    Raises:
+        TypeError: If text isn't a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
 
-		 t = 0
-		 while t < len(text) and text[t] == ' ':
-		 t += 1
+    t = 0
+    while t < len(text) and text[t] == ' ':
+        t += 1
 
-		 while t < len(text):
-			 print(text[t], end="")
-			 if text[t] == "\n" or text[t] in ".?:":
-			 if text[t] in ".?:":
-			 print("\n")
-			 t += 1
-			 while t < len(text) and text[t] == ' ':
-			 t += 1
-			 continue
-			 t += 1
-
+    while t < len(text):
+        print(text[t], end="")
+        if text[t] == "\n" or text[t] in ".?:":
+            if text[t] in ".?:":
+                print("\n")
+                t += 1
+                while t < len(text) and text[t] == ' ':
+                    t += 1
+                continue
+        t += 1
