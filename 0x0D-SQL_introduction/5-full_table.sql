4eaef3d449d34b09844e8333807e4ce610b880bd
Mery9yuka <benhaida.maryem@gmail.com>
2024-01-17 03:09:19 +0100

    Full description

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- prints the full description of the table first_table from hbtn_0c_0
+-- using SHOW CREATE TABLE we will be avle to display 
+-- the full statment that was used to create a table
+SHOW CREATE TABLE first_table;
