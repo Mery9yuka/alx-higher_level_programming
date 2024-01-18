cb69999c9f5e512f1f5645fd61f28da19686ddda
Mery9yuka <benhaida.maryem@gmail.com>
2024-01-18 03:54:43 +0100

    task 14

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,4 @@
---listing all genres from hbtn_0d_tvshows
--- displaying the number of shows linked to each
-SELECT genre AS genre, COUNT(*) AS number_of_shows
-FROM hbtn_0d_tvshows.shows
-JOIN hbtn_0d_tvshows.show_genres ON shows.id = show_genres.show_id
-JOIN hbtn_0d_tvshows.genres ON show_genres.genre_id = genres.id
-GROUP BY genre HAVING COUNT(*) > 0
-ORDER BY number_of_shows DESC;
+--listing all genres from hbtn_0d_tvshows & displaying nb of shows linked to each
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_genres RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre ORDER BY number_of_shows DESC;
