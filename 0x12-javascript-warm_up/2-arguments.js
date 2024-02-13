7b1f53001700242f20d2964089213eb1174157f0
Mery9yuka <benhaida.maryem@gmail.com>
2024-02-13 03:23:33 +0100

    args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const argsNum = process.argv.length - 2;
 if (argsNum === 0) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (argsNum === 1) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
