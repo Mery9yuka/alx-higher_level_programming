4e9e2a153fded888e2a7e5e4e1bde5129d94fb90
Mery9yuka <benhaida.maryem@gmail.com>
2024-03-02 02:18:10 +0100

    0&1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""script that fetches https://alx-intranet.hbtn.io/status using
-   urllib
+"""script that fetches https://alx-intranet.hbtn.io/status
+   using urllib
 """
 
 
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen(
+            'https://alx-intranet.hbtn.io/status') as response:
         cont = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(cont)))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""script that fetches https://alx-intranet.hbtn.io/status
+"""script that takes in a URL
+   -sends a request to the URL
+   -displays the value of the
+   X-Request-Id variable found in the header of the response
 """
 import sys
 import urllib.request
